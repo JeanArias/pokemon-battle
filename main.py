0277000929b195d812fc545fd740a1a41c15d6dc
María Fernanda Muñoz Vargas <maru.vragas.12@gmail.com>
2026-08-09 08:54:33 -0600

    Agregar Lucario
    Se realiza el pokemon de Lucario y se le agregan las características

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 from pokemons.pikachu import Pikachu
 from pokemons.charmander import Charmander
+from pokemons.lucario import Lucario
 
 from batalla import Batalla
 
 pikachu = Pikachu()
-charmander = Charmander()
+lucario = Lucario()
 
-batalla = Batalla(pikachu,charmander)
+batalla = Batalla(pikachu, lucario)
 
 batalla.combatir()
